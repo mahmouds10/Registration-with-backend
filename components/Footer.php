dd09b3cb8bcd388ed9397ec48fe9f43890a58946
Mahmoud Sayed <mahmoudsayed1612@gmail.com>
2024-04-04 15:00:36 +0200

    Create Footer

---
+++ components/Footer.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+    <link rel="stylesheet" href="css/footer.css">
+</head>
+
+<body>
+    <div class="container-fluid justify-content-center">
+        <div class="row py-3">
+            <div class="col">
+                <div class="card border-0">
+                    <div class="card-body text-center ">
+                        <h2><b>Let `s have a chat !</b></h2>
+                        <p class="pl-0 ml-0 mb-5">Find out what we can do for your business or home.</p>
+                        <div class="row text-center justify-content-center">
+                            <div class="col-auto">
+                                <div class="input-group-lg input-group mb-3 "><input type="text" class="form-control"
+                                        placeholder="Enter your e-mail address" aria-label="Recipient's username"
+                                        aria-describedby="button-addon2">
+                                    <div class="input-group-append"><button class="btn myBtn btn-success" type="button"
+                                            id="button-addon2"> <b>Submit</b></button></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="mx-0 px-0">
+        <footer>
+            <div class="row justify-content-around mb-0 pt-3 pb-0 ">
+                <div class=" col-11">
+                    <div class="row justify-content-center">
+                        <div class="col-md-3 col-12 font-italic align-items-center mt-md-3 mt-4">
+                            <h5 class="d-flex align-items-center ">
+                                <span> <img src="imgs/development.svg" class="img-fluid me-2 "></span><b
+                                    class="text-dark"> FCAI-<span class="text-muted">
+                                        CU</span></b>
+                            </h5>
+                            <p class="social mt-md-3 mt-2"> <span><i class="fa fa-facebook "
+                                        aria-hidden="true"></i></span> <span><i class="fa fa-linkedin"
+                                        aria-hidden="true"></i></span> <span><i class="fa fa-twitter"
+                                        aria-hidden="true"></i></span> </p> <small
+                                class="copy-rights cursor-pointer">&#9400; 2024 FCAI-CU Development</small><br>
+                            <small>Copyright.All Rights Resered. </small>
+                        </div>
+                        <div class="col-md-3 col-12 my-sm-0 mt-5">
+                            <ul class="list-unstyled">
+                                <li class="mt-md-3 mt-4">Our Solution</li>
+                                <li>Intergrated Security Platform</li>
+                                <li>Core Features</li>
+                                <li>Product Features</li>
+                                <li>Pricing</li>
+                            </ul>
+                        </div>
+                        <div class="col-md-3 col-12 my-sm-0 mt-5">
+                            <ul class="list-unstyled">
+                                <li class="mt-md-3 mt-4">Your needs</li>
+                                <li>Intergrated Security Platform</li>
+                                <li>Core Features</li>
+                                <li>Product Features</li>
+                                <li>Pricing</li>
+                            </ul>
+                        </div>
+                        <div class="col-xl-auto col-md-3 col-12 my-sm-0 mt-5">
+                            <ul class="list-unstyled">
+                                <li class="mt-md-3 mt-4">Offer</li>
+                                <li>Intergrated Security Platform</li>
+                                <li>Core Features</li>
+                                <li>Product Features</li>
+                                <li>Pricing</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+</body>
+
+</html>
